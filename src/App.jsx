403ebceceee1ebaf241b0ce126acefb53a5ba5bb
Wahyu Pratama <wahyulucky28@gmail.com>
2023-12-01 19:51:36 +0700

    fixing title

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className='min-h-screen min-w-full overflow-hidden bg-slate-200'>
       <div className='max-w-[1000px] mx-auto px-4 py-5'>
-        <h1 className='text-center font-semibold text-xl'>
-          Learning Tailwind with Reach
-        </h1>
+        <h1 className='text-center font-semibold text-xl'>Website Clone</h1>
+        <h3 className='text-center text-slate-500'>
+          Learn tailwind css by cloning websites
+        </h3>
 
         <div className='flex flex-wrap mt-[50px] md:grid md:grid-cols-3 gap-3'>
           <Link to={`shell`}>
